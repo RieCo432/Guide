fb67e62ce696723e83ec99cc0eafbccacd75c7ab
Twiistex <twiistex@gmail.com>
2016-01-11 23:34:30 +0100

    Advanced Design and Icons
    Using good icons and selected a gradient as background
    color.

---
+++ app/src/main/java/com/colinries/guide/ItemFragment.java
@@ -0,0 +1,42 @@
+package com.colinries.guide;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class ItemFragment extends Fragment {
+
+    public ItemFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.fragment_item, container, false);
+
+        return layout;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
--- app/src/main/java/com/colinries/guide/MainActivity.java
+++ app/src/main/java/com/colinries/guide/MainActivity.java
@@ -3,6 +3,8 @@ package com.colinries.guide;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,15 +24,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -39,6 +32,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Get and display main fragment
+        //TODO: fill fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new MainFragment()).commit();
+
 
     }
 
@@ -82,13 +82,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
         if (id == R.id.nav_howto) {
+            fragmentTransaction.replace(R.id.fragment_container, new HowToFragment()).commit();
+            setTitle(getString(R.string.how_tos));
 
         } else if (id == R.id.nav_items) {
+            fragmentTransaction.replace(R.id.fragment_container, new ItemFragment()).commit();
+            setTitle(getString(R.string.items));
 
         } else if (id == R.id.nav_faq) {
+            fragmentTransaction.replace(R.id.fragment_container, new FAQFragment()).commit();
+            setTitle(getString(R.string.faq));
 
         } else if (id == R.id.nav_about) {
+            fragmentTransaction.replace(R.id.fragment_container, new AboutFragment()).commit();
+            setTitle(getString(R.string.about));
 
         } else if (id == R.id.nav_support) {
 
