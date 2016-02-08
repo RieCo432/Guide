4d485bdea1b807f38b15b2c94ee6f74817e91150
Colin Ries <colin.ries@gmail.com>
2016-02-08 04:06:14 +0100

    Changed a lot :P
    Added all Item Fragments
    Added Nav Drawer Entries for badges
    Added Badges Activity
    Added TODO Statements
    Added Badges Name Strings

--- app/src/main/java/com/colinries/guide/BadgesActivity.java
+++ app/src/main/java/com/colinries/guide/BadgesActivity.java
@@ -1,11 +1,8 @@
 package com.colinries.guide;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.view.LayoutInflater;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,17 +12,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class BadgesActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_badges);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -37,18 +31,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        //Get and display main fragment
-        //TODO: fill fragment
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, new MainFragment()).commit();
-        setTitle(getString(R.string.main));
-
-
     }
-
-
 
     @Override
     public void onBackPressed() {
@@ -63,7 +46,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.badges, menu);
         return true;
     }
 
@@ -88,40 +71,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //TODO: Add all navigation entries and their respective fragments
 
-
-        if (id == R.id.nav_main) {
-            fragmentTransaction.replace(R.id.fragment_container, new HowToFragment()).commit();
-            setTitle(getString(R.string.how_tos));
-
-        } else if (id == R.id.nav_howto) {
-            fragmentTransaction.replace(R.id.fragment_container, new HowToFragment()).commit();
-            setTitle(getString(R.string.how_tos));
-
-        } else if (id == R.id.nav_items) {
-            Intent intent = new Intent(this, ItemActivity.class);
-            startActivity(intent);
-
-
-        } else if (id == R.id.nav_badges) {
-            Intent intent = new Intent(this, BadgesActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_faq) {
-            fragmentTransaction.replace(R.id.fragment_container, new FAQFragment()).commit();
-            setTitle(getString(R.string.faq));
-        } else if (id == R.id.nav_about) {
-            fragmentTransaction.replace(R.id.fragment_container, new AboutFragment()).commit();
-            setTitle(getString(R.string.about));
-        } else if (id == R.id.nav_support) {
-
-        } else if (id == R.id.nav_feedback) {
-
-        } else if (id == R.id.nav_settings) {
-
+        if (id == R.id.nav_verified) {
+            // Handle the camera action
         }
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
