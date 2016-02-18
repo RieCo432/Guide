e2c39b4a53f1d36d22b45d1d83726d984b78b95c
Colin Ries <colin.ries@gmail.com>
2016-02-19 00:38:14 +0100

    added FAQ XML parser + optimized imports
    + updated design in item IQ

--- app/src/main/java/com/colinries/guide/AboutFragment.java
+++ app/src/main/java/com/colinries/guide/AboutFragment.java
@@ -1,13 +1,18 @@
 package com.colinries.guide;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AboutFragment extends Fragment {
 
@@ -26,6 +31,38 @@ public class AboutFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.fragment_about, container, false);
+
+        String versionName = BuildConfig.VERSION_NAME;
+        String versionCode = Integer.toString(BuildConfig.VERSION_CODE);
+        String versionDate = BuildConfig.versionDate;
+
+        String[][] listData = {
+                {getString(R.string.app_name), getString(R.string.subheader)},
+                {getString(R.string.version), versionName},
+                {getString(R.string.build), versionCode},
+                {getString(R.string.builddate), versionDate},
+                {getString(R.string.developers), getString(R.string.developers_names)},
+                {getString(R.string.copyright), getString(R.string.copyright_label)}
+        };
+
+        ArrayList<HashMap<String, String>> list = new ArrayList<>();
+
+        final ListView aboutView = (ListView) layout.findViewById(R.id.aboutListView);
+
+        HashMap<String, String> item;
+        for (String[] aListData: listData) {
+            item = new HashMap<>();
+            item.put("line1", aListData[0]);
+            item.put("line2", aListData[1]);
+            list.add(item);
+        }
+
+        ListAdapter myListAdapter = new SimpleAdapter(getActivity(), list,
+                android.R.layout.simple_list_item_2,
+                new String[] {"line1", "line2"},
+                new int[] {android.R.id.text1, android.R.id.text2});
+
+        aboutView.setAdapter(myListAdapter);
 
         return layout;
     }
--- app/src/main/java/com/colinries/guide/BadgesActivity.java
+++ app/src/main/java/com/colinries/guide/BadgesActivity.java
@@ -1,9 +1,6 @@
 package com.colinries.guide;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
